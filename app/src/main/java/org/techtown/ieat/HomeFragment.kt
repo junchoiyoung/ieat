47a352fb3e21cf1390e15a267c98794d8edd17fe
js009 <tnstls02@naver.com>
2023-05-25 15:38:26 +0900

    0.3

--- app/src/main/java/org/techtown/ieat/HomeFragment.kt
+++ app/src/main/java/org/techtown/ieat/HomeFragment.kt
@@ -1,31 +1,70 @@
 package org.techtown.ieat
 
+import android.Manifest.permission_group.CAMERA
+import android.Manifest.permission_group.STORAGE
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Camera
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+lateinit var bitmap: Bitmap
+lateinit var imageView: ImageView
+lateinit var imageView2: ImageView
+private val cameraPermissionRequestCode = 1001
+private val cameraRequestCode = 1002
+private const val STORAGE_CODE = 99
+private val OPENGALLERY = 1
+
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+    }
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        if(it.resultCode == RESULT_OK && it.data !== null){
+            //값 담기
+            val extras = it.data!!.extras
+            //비트맵 으로 타입 변경
+            bitmap = extras?.get("data") as Bitmap
+            //화면에 보여주기
+            imageView.setImageBitmap(bitmap)
         }
     }
 
@@ -33,20 +72,92 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        imageView2 = view.findViewById(R.id.gallery_view)
+        imageView = view.findViewById(R.id.avatars)
+        val picBtn = view.findViewById<ImageButton>(R.id.btn_camera)
+        val galleryBtn = view.findViewById<ImageButton>(R.id.btn_gallery)
+
+        //카메라버튼클릭
+        picBtn.setOnClickListener {
+            requestCameraPermission()
+        }
+        galleryBtn.setOnClickListener{
+            openGallery()
+        }
+
+        return view
+    }
+    //카메라 권한 받기
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.CAMERA),
+                cameraPermissionRequestCode
+            )
+        } else {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, cameraRequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequestCode && resultCode == Activity.RESULT_OK && data != null) {
+            val extras = data.extras
+            bitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bitmap)
+        }
+
+        else if (requestCode == STORAGE_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri: Uri? = data.data
+            if (imageUri != null) {
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imageUri)
+                    imageView2.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            Log.d("ActivityResult", "Something went wrong")
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == cameraPermissionRequestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "카메라 권한이 거부되었습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun openGallery(){
+        val intent: Intent =Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, OPENGALLERY)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
--- app/src/main/java/org/techtown/ieat/MainActivity.kt
+++ app/src/main/java/org/techtown/ieat/MainActivity.kt
@@ -1,10 +1,23 @@
 package org.techtown.ieat
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import org.techtown.ieat.databinding.ActivityMainBinding
+import java.io.File
 
 private const val TAG_HOME = "home_fragment"
 private const val TAG_RECIPE = "recipe_fragment"
@@ -12,11 +25,14 @@ private const val TAG_MYRECIPE = "myrecipe_fragment"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
 
         setFragment(TAG_HOME, HomeFragment())
 
@@ -29,7 +45,9 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+
     }
+
 
     private fun setFragment(tag:String, fragment: Fragment) {
         val manager: FragmentManager = supportFragmentManager
